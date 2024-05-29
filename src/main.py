58a30a4d038630adbd94f660022681d825b525ad
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-05-29 11:42:02 +0200

    feat(wip): add cors

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import message
 
 app = FastAPI(
     title="AI Backend",
     description="ai backend for your app powered by llama3",
-    version="0.1.0"
+    version="0.1.0",
 )
+
+origins = [
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(message.router)
