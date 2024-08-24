fa7f51846b3d2acea56848334f85c933d261c541
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-08-24 22:22:24 +0200

    feat: update config

--- examples/web-nuxt/eslint.config.mjs
+++ examples/web-nuxt/eslint.config.mjs
@@ -1,41 +1,4 @@
 import withNuxt from "./.nuxt/eslint.config.mjs"
-import eslintConfigPrettier from "eslint-config-prettier"
+import config from "@dkupinic/eslint-config"
 
-export default withNuxt(
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    rules: {
-      curly: ["warn", "multi-line"],
-      eqeqeq: "error",
-      yoda: "warn",
-      "prefer-promise-reject-errors": ["error", { allowEmptyReject: true }],
-      "array-callback-return": ["error", { allowImplicit: true }],
-      "no-else-return": "warn",
-      "no-console": "error",
-      "no-constant-binary-expression": "error",
-      "no-useless-return": "warn",
-      "default-case-last": "error",
-      "no-promise-executor-return": "error",
-      "no-template-curly-in-string": "error",
-      "no-unreachable-loop": "error",
-      "no-duplicate-imports": "warn",
-      "no-multiple-empty-lines": "warn",
-      "no-label-var": "error",
-      "no-undef-init": "warn",
-      "no-unused-vars": [
-        "error",
-        {
-          args: "after-used",
-          argsIgnorePattern: "^_",
-          ignoreRestSiblings: false,
-          vars: "all",
-          varsIgnorePattern: "^_",
-        },
-      ],
-    },
-  },
-  {
-    ignores: ["**/components/ui", "**/components/content", "**/lib"],
-  },
-  eslintConfigPrettier,
-)
+export default withNuxt(...config)
