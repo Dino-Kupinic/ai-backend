685e45afa1937ddb267dcf738c6f03791d8fd08e
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2025-03-24 17:30:41 +0100

    feat: work on migration

--- examples/web-nuxt/nuxt.config.ts
+++ examples/web-nuxt/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/eslint"],
+  css: ['~/assets/css/main.css'],
   components: [
     {
       path: "~/components",
