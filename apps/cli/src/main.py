0b272b07cc1531f6ebdc3d2ee2d5f2003312cda8
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2025-03-31 18:05:39 +0200

    feat: start and stop works

--- apps/cli/src/main.py
+++ apps/cli/src/main.py
@@ -1,3 +1,7 @@
+import os
+import signal
+import sys
+
 import click
 import subprocess
 from pathlib import Path
@@ -6,17 +10,53 @@ from pathlib import Path
 def cli() -> None:
     pass
 
+
+PID_FILE = Path("server.pid")
+"""File to store if the server is running."""
+
 @cli.command()
 def start() -> None:
     """Start the API backend server"""
     api_path = Path(__file__).parent.parent.parent / 'api' / 'src'
     try:
-        subprocess.run(['uvicorn', 'apps.api.src.main:app', '--reload'], cwd=str(api_path))
-    except KeyboardInterrupt:
-        click.echo('\nStopping API server...')
+        api_process = subprocess.Popen(
+            ['fastapi', 'run', 'main.py'],
+            cwd=str(api_path),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        pid = api_process.pid
+
+        with open(PID_FILE, "w") as f:
+            f.write(str(pid))
+
+        click.echo(f'\nStarted API server with {pid}')
+
+        for line in iter(api_process.stdout.readline, ""):
+            sys.stdout.write(line)
+            sys.stdout.flush()
     except Exception as e:
         click.echo(f'Error starting API server: {e}')
 
+@cli.command()
+def stop() -> None:
+    """Stop the API backend server"""
+    if not PID_FILE.exists():
+        click.echo('API server is not running.')
+        return
 
-if __name__ == '__main__':
+    try:
+        with open(PID_FILE, "r") as f:
+            pid = int(f.read().strip())
+
+        os.kill(pid, signal.SIGTERM)
+
+        PID_FILE.unlink()
+        click.echo(f"API server with PID {pid} stopped.")
+    except Exception as e:
+        click.echo(f"Error stopping API server: {e}")
+
+
+if __name__ == "__main__":
     cli()
